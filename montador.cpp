5f586fb342600d879fd96b61e74150bef6e1fa38
enzo yoshio <enzoyn@hotmail.com>
2022-12-26 19:41:30 -0300

    adicionando o arquivo principal e talz

---
+++ montador.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char *argv[]) {
+
+  // se nao passar nenhum argumento para o exec
+  if(argc == 1) {
+    std::cout << "informe uma opcao e um arquivo para ser montado" << std::endl;
+    return 0;
+  }
+
+  if(argc == 2) {
+    std::cout << "informe o arquivo a ser montado, sem extensao" << std::endl;
+    return 0;
+  }
+
+  switch(argv[1][1]) {
+    case 'p':
+      std::cout << "pre processamento do arquivo " << argv[2] << std::endl;
+      break;
+    case 'm':
+      std::cout << "processamento de macros do arquivo " << argv[2] << std::endl;
+      break;
+    case 'o':
+      std::cout << "criando arquivo objeto do arquivo " << argv[2] << std::endl;
+      break;
+    // se der tempo fazer um help
+    // bem simples por que so tem tres opcoes
+    case 'h':
+      std::cout << "help" << std::endl;
+      break;
+    default:
+      std::cout << "opcao nao existente, para saber quais opcoes estao disponiveis use './montador -h'" << std::endl;
+
+  }
+}
